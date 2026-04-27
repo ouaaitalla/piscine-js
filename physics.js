2d602ce5a3ffa83dc19744a7eef8645666871f13
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-27 14:24:36 +0100

    physics

--- physics.js
+++ physics.js
@@ -2,8 +2,8 @@ function getAcceleration(obj) {
     if (obj.f !== undefined && obj.m !== undefined) {
         return obj.f / obj.m
     }
-    if (obj.v !== undefined && obj.t !== undefined) {
-        return obj.v / obj.t
+    if (obj.Δt !== undefined && obj.Δt !== undefined) {
+        return obj.Δv / obj.Δt
     }
     if (obj.d !== undefined && obj.t !== undefined) {
         return 2 * obj.d / (obj.t * obj.t)
