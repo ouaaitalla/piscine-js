bc25e22a04a000864023c5dcba31c099726831c6
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-27 10:27:24 +0100

    mutability

--- mutability.js
+++ mutability.js
@@ -1,9 +1,9 @@
-// const persone= {
-//     name : 'rick',
-//     age : 77,
-//     country : 'FR'
+const persone= {
+    name : 'rick',
+    age : 77,
+    country : 'FR'
 
-// }
+}
 const clone1 = {...persone}
 const clone2 = {...persone}
 const samepersone = persone
