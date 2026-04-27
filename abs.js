f07cb17b31d57b2e4d6fb5bb5dd0df0f59251aad
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-27 13:46:06 +0100

    abs

---
+++ abs.js
@@ -0,0 +1,12 @@
+function isPositive(number) {
+    if (number > 0) {
+        return true
+    }
+    return false
+}
+function abs(number) {
+    if (number < 0) {
+        return -number
+    }
+    return number
+}
