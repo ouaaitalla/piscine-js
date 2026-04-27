c7752975017bb463fe7233a93298db31875fa48a
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-27 14:50:22 +0100

    collection

---
+++ collections.js
@@ -0,0 +1,49 @@
+function arrToSet(arr) {
+    return new Set(arr)
+}
+function arrToStr(arr) {
+    return arr.join('')
+}
+function setToArr(set) {
+    return Array.from(set)
+}
+function setToStr(set) {
+    return Array.from(set).join('')
+}
+function strToArr(str) {
+    return str.split('')
+}
+function strToSet(str) {
+    return new Set(str.split(''))
+}
+function mapToObj(map) {
+    return Object.fromEntries(map)
+}
+function objToArr(obj) {
+    return Object.entries(obj)
+}
+function objToMap(obj) {
+    return new Map(Object.entries(obj))
+}
+function arrToObj(arr) {
+    return Object.fromEntries(arr)
+}
+function strToObj(str) {
+    return Object.fromEntries(str.split('').map((char, index) => [index, char]))
+}
+
+function superTypeOf(value) {
+    if (value === null) {
+        return 'null'
+    }
+    if (Array.isArray(value)) {
+        return 'array'
+    }
+    if (value instanceof Map) {
+        return 'map'
+    }
+    if (value instanceof Set) {
+        return 'set'
+    }
+}
+
