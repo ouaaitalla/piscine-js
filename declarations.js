0a32c08d61179b571fcc93aeb376100b785d246c
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-27 09:54:45 +0100

    declaration

---
+++ declarations.js
@@ -0,0 +1,19 @@
+const escapeStr = ", , /, "
+const arr = [4, 2]
+const obj = {
+    str : "",
+    num : 0,
+    bool : false ,
+    undef : undefined,
+}
+const nested ={
+    arr : [4, undefined, 2],
+    obj : {
+        str : "hello",
+        num : 1,
+        bool : false ,
+    }
+}
+Object.freeze(nested)
+Object.freeze(obj)
+Object.freeze(arr)
