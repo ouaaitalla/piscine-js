423add9a0425e732fbd4a654433dcb7159ced1b8
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-27 10:36:08 +0100

    mutability

--- mutability.js
+++ mutability.js
@@ -1,12 +1,7 @@
-const person = {
-  name: 'Rick',
-  age: 77,
-  country: 'US',
-}
 
-const clone1 = {...persone}
-const clone2 = {...persone}
-const samepersone = persone
+const clone1 = { ...persone};
+const clone2 = { ...persone};
+const samepersone = persone;
 
-persone.age++
-persone.country='FR'
+persone.age++;
+persone.country='FR';
