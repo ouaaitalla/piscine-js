71838490eb289ac505721cb9f31a08d659d27f48
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-29 16:37:15 +0100

    pyramid

---
+++ pyramid.js
@@ -0,0 +1,12 @@
+function pyramid (str , num) {
+    let result = '';
+    for (let i = 1; i <= num; i++) {
+        result += ' '.repeat(num - i) + str.repeat(2 * i - 1);
+        if (i < num) {
+            result += '\n';
+        }
+    }
+    return result;
+}
+
+console.log(pyramid('x', 5));
