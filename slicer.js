c2898b7682f6153723b4bbacf81f6de2ee4c7b60
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-29 10:27:42 +0100

    slice

---
+++ slicer.js
@@ -0,0 +1,19 @@
+function slice (str, start, end) {
+    let sign = false
+
+    if (start < 0) {
+        start = str.length + start
+    }
+    if (end < 0) {
+        end = str.length + end
+    }
+
+    if (start >= end) {
+        return ''
+    }
+    let result = ''
+    for (let i = start; i < end && i < str.length; i++) {
+        result += str[i]
+    }
+    return result
+}
