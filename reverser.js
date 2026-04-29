b8e493bcfc4a9a49c5de0365717d111919a4993b
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-29 14:37:07 +0100

    reverse

--- reverser.js
+++ reverser.js
@@ -1,9 +1,18 @@
 function reverse(str) {
-    let reverse = [];
-    for (let i = str.length - 1; i >= 0; i--) {
+    if (Array.isArray(str)) {
+      let reverse = [];
+        for (let i = str.length - 1; i >= 0; i--) {
         reverse.push(str[i]);
     }
 
-    return reverse;
+        return reverse;
+    }
+    if (typeof str === 'string') {
+        let reverse = '';
+        for (let i = str.length - 1; i >= 0; i--) {
+        reverse += str[i];  
+    }
+        return reverse;
+    }
 }
 
