7149f1b5331a997e264e7a8eb3b5d988ca9d32eb
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-29 10:43:15 +0100

    slice

---
+++ keep-cut.js
@@ -0,0 +1,73 @@
+function cutFirst (str) {
+    if (str.length === 0) {
+        return ''
+    }
+    let result = ''
+    for (let i = 2; i < str.length; i++) {
+        result += str[i]
+    }
+    return result
+}
+
+function cutLast (str) {
+    if (str.length === 0) {
+        return ''
+    }
+    let result = ''
+    for (let i = 0; i < str.length - 2; i++) {
+        result += str[i]
+    }
+    return result
+}
+
+function cutFirstLast (str) {
+    if (str.length === 0) {
+        return ''
+    }
+    let result = ''
+    for (let i = 2; i < str.length - 2; i++) {
+        result += str[i]
+    }
+    return result
+}
+
+function keepFirst (str) {
+    if (str.length === 0) {
+        return ''
+    }
+    let result = ''
+    for (let i = 0; i < 2 && i < str.length; i++) {
+        result += str[i]
+    }
+    return result
+}
+
+function keepLast (str) {
+    if (str.length === 0) {
+        return ''
+    }
+    let result = ''
+    for (let i = str.length - 2; i < str.length; i++) {
+        if (i >= 0) {
+            result += str[i]
+        }
+    }
+    return result
+}
+
+function keepFirstLast (str) {
+    if (str.length === 0) {
+        return ''
+    }
+    let result = ''
+    for (let i = 0; i < 2 && i < str.length; i++) {
+        result += str[i]
+    }
+    for (let i = str.length - 2; i < str.length; i++) {
+        if (i >= 0) {
+            result += str[i]
+        }
+    }
+    return result
+}
+
