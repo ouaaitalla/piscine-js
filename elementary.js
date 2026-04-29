57893744c10be6c2de6e8f7f7234149f5af7f1d2
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-29 09:11:50 +0100

    operation

--- elementary.js
+++ elementary.js
@@ -1,31 +1,58 @@
 function multiply (a, b) {
     let result = 0
     let i = 0
+    let sign = 1
+    if (a < 0) {
+        a = -a
+        sign = -sign
+    }
+    if (b < 0) {
+        b = -b
+        sign = -sign
+    }
     while (i < b) {
         result += a
         i++
     }
-    return result
+    return sign * result
 }
 
 function divide (a, b) {
     let result = 0
     let modul = 0
+    let sign = 1
+    if (a < 0) {
+        a = -a
+        sign = -sign
+    }
+    if (b < 0) {
+        b = -b
+        sign = -sign
+    }
     while (a >= b) {
         a -= b
         result++
     }
     modul = a
-    return  result
+    return  sign * result
 }
 
 function modulo (a, b) {
     let dived = 0
     let modul = 0
+    let sign = 1
+    if (a < 0) {
+        a = -a
+        sign = -sign
+    }
+    if (b < 0) {
+        b = -b
+        sign = -sign
+    }
     while (a >= b) {
         a -= b
         dived++
     }
     modul = a
-    return  modul
+    return  sign * modul
 }
