7d4a7920118d798b054f487b29c4a4270b650c67
ouaaitalla <houssamaitalla5@gmail.com>
2026-04-30 11:33:54 +0100

    flat

---
+++ find-expression.js
@@ -0,0 +1,20 @@
+const add4 = '+4'
+const mul2 = '*2'
+
+function findExpression (number) {
+    if (number <= 0) {return undefined;}
+    if (number%2 != 0) {return undefined;}
+    let result = "1";
+    let current = 1;
+    if (number === 1) {return result;}
+    while (current < number) {
+        if ((current+4) %4 === 0) {
+            result += add4;
+            current += 4;
+        }else {
+            result += mul2;
+            current *= 2;
+        }
+    }
+    return result;
+}
