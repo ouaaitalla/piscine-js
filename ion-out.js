8a71c56e85544128778add8254cc46a1ba4ffa51
Oualid Ait alla <houssamaitalla5@gmail.com>
2026-05-01 12:11:26 +0100

    regex

--- ion-out.js
+++ ion-out.js
@@ -2,8 +2,6 @@ function ionOut(str){
     let exp = /\w*tion\w*/g
     let exp1 = new RegExp("tion","g")
     let words = str.match(exp)
-    console.log(words)
+    if (words === null){return []}
     return words.map(e =>e.replace(exp1,"t"))
 }
-
-  console.log(ionOut("function sa3id nion propaganda passion"))
