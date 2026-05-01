a38679e32b3b9d364945c2423c976a7dcfe6b4a6
Oualid Ait alla <houssamaitalla5@gmail.com>
2026-05-01 11:26:22 +0100

    regex

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,11 @@
 function sameAmount(str , rexp1, rexp2){
-    let arr1 = str.match(rexp1)
+    let arr1 = str.match(new RegExp(rexp1 ,"g"))
     if (arr1 === null) {arr1 = []}
-    let arr2 = str.match(rexp2)
-    if (arr1 === null) {arr1 = []}
+    let arr2 = str.match(new RegExp(rexp2,"g"))
+    console.log(arr1)
+    
+    if (arr2 === null) {arr2 = []}
+    console.log(arr2)
     if (arr1.length === arr2.length){
         return true 
     }else{
