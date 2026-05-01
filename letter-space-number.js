24beeb06d9e24d8eddfa8eb4ff38e9403c0b8825
Oualid Ait alla <houssamaitalla5@gmail.com>
2026-05-01 10:34:27 +0100

    regex

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,4 +1,7 @@
 function letterSpaceNumber(str){
-    const he = new RegExp("[a-zA-Z] [0-9](?=[^0-9A-Za-z])", "g")
-    return str.match(he)
+    const he = new RegExp("[a-zA-Z] [0-9](?![^0-9A-Za-z])", "g")
+    let arr = str.match(he)
+    return arr === null ? [] : arr
 }
+
+console.log(letterSpaceNumber('I like 7up.'))
