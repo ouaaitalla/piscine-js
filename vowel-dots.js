b0c237d1558d7f49e3716dd7d8ecaee9d6b36cec
Oualid Ait alla <houssamaitalla5@gmail.com>
2026-05-01 10:54:50 +0100

    regex

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,6 +1,7 @@
+const vowels = new RegExp("[aeiuo]" , "g");
+
 function vowelDots(str){
-    const vowels = new RegExp("[aeiuo]" , "g");
+
     return str.replace(vowels,'$&.');
 } 
 
-console.log(vowelDots("hey a berra ss"))
