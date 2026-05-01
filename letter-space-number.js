940ecb844e4c5fa3a262c8e1890ad1f0d45d1e07
Oualid Ait alla <houssamaitalla5@gmail.com>
2026-05-01 10:36:49 +0100

    regex

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,7 @@
 function letterSpaceNumber(str){
-    const he = new RegExp("[a-zA-Z] [0-9](?![^0-9A-Za-z])", "g")
+    const he = new RegExp("[a-zA-Z] [0-9](?![0-9A-Za-z])", "g")
     let arr = str.match(he)
     return arr === null ? [] : arr
 }
 
-console.log(letterSpaceNumber('I like 7up.'))
+console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
