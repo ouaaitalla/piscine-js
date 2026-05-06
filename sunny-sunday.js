bac9fdb37ed11e9c7539a4082c2ebe0a7110c1e8
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-06 16:23:51 +0100

    map

---
+++ sunny-sunday.js
@@ -0,0 +1,3 @@
+export function sunnySunday(date){
+    
+}
---
+++ using-map.js
@@ -0,0 +1,42 @@
+
+export function citiesOnly(arr){
+    return arr.map(obj=>obj.city)
+}
+
+export function upperCasingStates(arr) {
+    return arr.map(function(str){
+        let sting = str.split(" ")
+        return sting.map(function(str){
+            let word = str[0].toUpperCase() + str.slice(1).toLowerCase()
+            return word
+        }).join(" ")
+    })
+
+}
+
+export function fahrenheitToCelsius(arr){
+    let celarr = arr.map(function(str){
+        let daraja = str.slice(0,-2)
+        return Math.round((daraja -32)/1.8)
+    })
+    let cel = celarr.map(function(cell){
+         return String(cell)+"°C"
+    })
+    return cel
+}
+
+export function trimTemp(arr){
+    return arr.map(function(obj){
+        obj.temperature =  obj.temperature.replace(/\s/g,"")
+        return obj
+    })
+}
+
+export function tempForecasts(arr){
+    arr = trimTemp(arr)
+    let temp = arr.map(function(obj){return obj.temperature })
+    let cel = fahrenheitToCelsius(temp)
+    let state = arr.map(obj=>obj.state)
+    return String(cel)+"elsius in "+ citiesOnly(arr)+", "+state
+}
+
