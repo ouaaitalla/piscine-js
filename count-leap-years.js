0ae1686cd131ab829af002ace2e3522b333ab813
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-06 12:40:22 +0100

    date

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,7 +1,11 @@
 export function countLeapYears(date){
     const year = new Date(date).getFullYear()
-
-    return Math.floor(year / 4)
+    let res = Math.floor(year / 4)
          - Math.floor(year / 100)
-         + Math.floor(year / 400)
+         + Math.floor(year / 400)-1
+    if (res <0 ){
+        return 0
+    }
+    return res
 }
+console.log(countLeapYears(new Date('0001-12-01')))
