dd082d0d4c38fc4189bb06382d90ec61716c79ad
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-07 16:45:44 +0100

    map

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -6,26 +6,32 @@ export function build(num){
             return
         }
         const brick = document.createElement('div')
-        brick.id =`btick-${count}`
+        brick.id =`brick-${count}`
         if (count % 3  === 2){
             brick.dataset.foundation = 'true'
         }
-        document.body.appendChild(brick)
+        document.body.append(brick)
         count++
 
-    },100)
+    },1)
 }
 
-export function repair(ids){
-    ids.forEach(function(id){
-        const brick = document.getElementById(id)
+export function repair(...ids){
+    for(let id of ids){
+           const brick = document.getElementById(id)
+        console.log(brick,id)
         if (!brick) return
+
         if (brick.dataset.foundation === 'true'){
+       console.log("yes",id)
             brick.dataset.repaired = 'in progress'
         }else{
             brick.dataset.repaired = 'true'
-        }
-    })
+        } 
+    }
+    // ids.forEach(function(id){
+    
+    // })
 }
 
 export function destroy(){
