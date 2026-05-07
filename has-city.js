e5ea10241bdf169a1e8fe45180994b1f4d34a89e
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-07 13:50:47 +0100

    map

--- has-city.js
+++ has-city.js
@@ -1,8 +1,8 @@
 function hasCity(country, arr){
     let st = new Set(arr)
     return function(city){
-        for(cit of st){
-            if (cit === city){
+        for(let cit of st){
+            if ( cit === city){
                 return city +" is a city from "+country
             }
                 return city+ " is not a city from "+country
