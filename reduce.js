1ca1d64a280724b4958f36806fc1adb711b529ce
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-07 12:03:58 +0100

    map

--- reduce.js
+++ reduce.js
@@ -29,10 +29,14 @@ function reduceRight(arr, func){
     if (arr.length < 1) {
         throw new Error("Empty array")
     }
-    let acc = arr[0]
-    for (let i = arr.length-1; i >= 0 ; i--){
+    let acc = arr[arr.length-1]
+    for (let i = arr.length-2; i >= 0 ; i--){
         acc = func(acc, arr[i])
     }
 
     return acc
 }
+
+
+const adder = (a, b) => a + b
+console.log(reduceRight([1, 2, 3], adder))
