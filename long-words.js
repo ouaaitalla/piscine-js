0da125554db8071234e36cb9104bb73b08c29689
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-07 10:36:03 +0100

    map

---
+++ long-words.js
@@ -0,0 +1,17 @@
+function longWords(arr){
+    return arr.every(function(ele){
+        return ele.length >= 5
+    })
+}
+
+function oneLongWord(arr){
+    return arr.some(function(ele){
+        return typeof ele === 'string' && ele.length >= 10
+    })
+}
+
+function noLongWords(arr){
+    return arr.every(function(elem){
+        return !(typeof ele === 'string' && ele.length >= 7)
+    })
+}
