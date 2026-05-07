4a6fe427f5a2ecb78f78ccda0ebfb7c536c90dcd
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-07 11:54:13 +0100

    map

---
+++ reduce.js
@@ -0,0 +1,38 @@
+function fold(arr, func, acc){
+    for(let i = 0; i < arr.length; i++){
+        acc = func(acc, arr[i])
+    }
+    return acc
+}
+
+function foldRight(arr, func, acc){
+    for(let i = arr.length - 1; i >= 0; i--){
+        acc = func(acc, arr[i])
+    }
+    return acc
+}
+
+
+function reduce(arr, func){
+    if (arr.length < 1) {
+        throw new Error("Empty array")
+    }
+    let acc = arr[0]
+    for (let i = 1; i < arr.length; i++){
+        acc = func(acc, arr[i])
+    }
+
+    return acc
+}
+
+function reduceRight(arr, func){
+    if (arr.length < 1) {
+        throw new Error("Empty array")
+    }
+    let acc = arr[0]
+    for (let i = arr.length-1; i >= 0 ; i--){
+        acc = func(acc, arr[i])
+    }
+
+    return acc
+}
