f822852e2b12384c86dc68e1878e8809705febef
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-07 11:11:32 +0100

    map

---
+++ mapper.js
@@ -0,0 +1,25 @@
+function map(arr, func){
+    let res = []
+    for(let i = 0; i < arr.length; i++){
+        res.push  = func(arr[i],i,arr)
+    }
+    return res 
+}
+
+function flat(arr , depth = 1) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i]) && depth > 0) {
+            res.push(...flat(arr[i], depth - 1))
+        } else {
+            res.push(arr[i])
+        }
+    }
+    return res;
+}
+
+function flatMap(arr , func){
+    let pre = arr.map(func)
+    return flat(pre) 
+}
+
