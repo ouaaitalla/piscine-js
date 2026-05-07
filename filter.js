a9236b8c98dd4d670f436cd3af39e819fd7a7649
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-07 11:42:19 +0100

    map

--- filter.js
+++ filter.js
@@ -1,7 +1,9 @@
 function filter(arr, func){
     let res = []
     for(let i = 0; i < arr.length; i++){
-        res.push(func(arr[i],i,arr))
+        if (func(arr[i],i,arr)){
+            res.push(arr[i])
+        }
     }
     return res 
 }
@@ -9,9 +11,11 @@ function filter(arr, func){
 function reject(arr, func){
     let res = []
     for(let i = 0; i < arr.length; i++){
-        res.push(!func(arr[i],i,arr))
+        if (func(arr[i],i,arr)){
+            res.push(arr[i])
+        }
     }
-    return res
+    return res 
 }
 
 function partition(arr,func){
