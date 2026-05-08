d1e9451d67b8a72f59228b0b31a6b7855967c599
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-08 20:31:42 +0100

    tobedeleted v2

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,14 +1,14 @@
 export function generateLetters(){
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for (let i = 11 ; i <= 130 ; i++){
+    for (let i = 0 ; i <= 120 ; i++){
         const div = document.createElement('div')
         let random = Math.floor(Math.random()*letters.length)
         div.textContent = letters[random]
-        div.style.fontSize = i + 'px'
-        if (i <= 50){
+        div.style.fontSize = (11 +i) + 'px'
+        if (i <= 40){
             div.style.fontWeight = 300
         }
-        if (i <= 90){
+        if (i <= 80){
             div.style.fontWeight = 400
         }else{
             div.style.fontWeight = 600 
