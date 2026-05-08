9c76d1279e64e2f0efb4c22452c4a28d363e879c
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-08 20:29:49 +0100

    tobedeleted

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -6,12 +6,12 @@ export function generateLetters(){
         div.textContent = letters[random]
         div.style.fontSize = i + 'px'
         if (i <= 50){
-            div.style.fontWeight = 300 + 'px'
+            div.style.fontWeight = 300
         }
         if (i <= 90){
-            div.style.fontWeight = 400 + 'px'
+            div.style.fontWeight = 400
         }else{
-            div.style.fontWeight = 600 + 'px'
+            div.style.fontWeight = 600 
         }
         document.body.append(div)
     }
