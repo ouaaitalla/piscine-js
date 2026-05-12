849ff54ef485ccdeadc6b6648a24d015ad73419a
ouaaitalla <houssamaitalla5@gmail.com>
2026-05-12 19:10:04 +0100

    elem

--- ion-out.js
+++ ion-out.js
@@ -1,7 +1,8 @@
 function ionOut(str){
-
+    let exp = /\w*tion\w*/g
     let exp1 = new RegExp("tion","g")
     let words = str.match(exp)
     if (words === null){return []}
     return words.map(e =>e.replace(exp1,"t"))
 }
+
